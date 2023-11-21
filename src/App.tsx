5e5fd3e912ffcd7a5e496a3f3cadcc6e67036ebf
Brillitt <brillitt612@gmail.com>
2023-11-21 14:13:40 -0500

    commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,15 @@
 import Navbar from "./components/ui/organismos/Navbar/Navbar";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home/Home";
+import BookDetails from "./pages/BookDetails/BookDetails";
 
 function App() {
   return (
     <Router>
       <Navbar />
       <Routes>
-        <Route path="/" Component={Home} />
+        {/*<Route path="/" Component={Home} />*/}
+        <Route path="/" Component={BookDetails} />
         {/* Aqui ponen las rutas de las paginas */}
       </Routes>
     </Router>
---
+++ src/models/Book.model.ts
@@ -0,0 +1,16 @@
+export interface Book {
+    id: number;
+    titulo: string;
+    categoria:string[];
+    estado: string;
+    ISBN: string;
+    idioma: string;
+    numero_paginas: number;
+    fecha_publicacion: string;
+    editorial:string;
+    autor: string;
+    precio:number;
+    imagen_url: string;
+    encuadernacion:string;
+    sinopsis:string
+}
