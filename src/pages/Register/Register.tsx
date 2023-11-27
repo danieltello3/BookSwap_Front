97d61bc4fc820d2c6f1d5e29d34e6a3ee36c84c8
Alexander Cruz <amatu85@gmail.com>
2023-11-26 22:11:02 -0500

    update login

--- src/pages/Register/Register.tsx
+++ src/pages/Register/Register.tsx
@@ -1,27 +1,76 @@
 import React, { useState } from 'react';
-import { FaUser, FaEnvelope, FaLock } from 'react-icons/fa';
+import { FaUser, FaEnvelope, FaLock, FaIdCard, FaPhoneSquareAlt } from 'react-icons/fa';
 import Button from '../../components/ui/atomos/Button/Button';
-import { Link } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
+import apiRegister from './apiRegister';
 
 
 const Register = () => {
-    const [nombre, setNombre] = useState('');
-    const [apellido, setApellido] = useState('');
-    const [email, setEmail] = useState('');
-    const [password, setPassword] = useState('');
-    const [confirmPassword, setConfirmPassword] = useState('');
+    const [nombre, setNombre] = useState<string>('');
+    const [apellido, setApellido] = useState<string>('');
+    const [correo, setCorreo] = useState<string>('');
+    const [telefono, setTelefono] = useState<string>('');
+    const [dni, setDni] = useState<string>('');
+    const [password, setPassword] = useState<string>('');
+    const [confirmPassword, setConfirmPassword] = useState<string>('');
 
-    const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    // Inicializa la función navigate
+    const navigate = useNavigate();
+
+    const handleSubmit = async (e: React.FormEvent<HTMLFormElement>): Promise<void> => {
         e.preventDefault();
-
-        // Validar los campos (puedes agregar más validaciones según tus necesidades)
-        if (!nombre || !apellido || !email || !password || !confirmPassword) {
-            alert('Por favor, completa todos los campos.');
-            return;
+    
+        try {
+            // Validar los campos (puedes agregar más validaciones según tus necesidades)
+            if (!nombre || !apellido || !correo || !password || !confirmPassword) {
+                throw new Error('Por favor, completa todos los campos.');
+            }
+    
+            // Hacer la solicitud a la API utilizando la función importada
+            const { error, result } = await apiRegister({
+                nombre,
+                apellido,
+                correo,
+                telefono,
+                dni,
+                password,
+            });
+    
+            console.log(result); // Agrega esta línea para imprimir la respuesta en la consola
+    
+            if (error) {
+                // Manejar el error específico
+                throw new Error(error);
+            }
+    
+            // Verificar el código de respuesta de la API
+            if (result.success) {
+                // Usuario registrado con éxito
+                alert('Usuario registrado con éxito.');
+    
+                // Verificar si hay un mensaje en la respuesta (ajusta esto según la estructura de tu respuesta)
+                if ((result as any).message) {
+                    console.log('Mensaje del servidor:', (result as any).message);
+                }
+    
+                // Utilizar navigate para redirigir a la página de inicio de sesión
+                navigate('/Login');
+            } else {
+                // Manejar otros códigos de respuesta, por ejemplo, mostrar un mensaje de error
+                if (result.content && result.content.id === 'usuario_ya_registrado') {
+                    // Personaliza el mensaje para el caso de usuario ya registrado
+                    throw new Error('El correo ya está registrado. Por favor, usa otro correo.');
+                } else {
+                    // Mensaje de error genérico
+                    throw new Error(`Error al registrar usuario. Código de estado: ${result.status}`);
+                }
+            }
+        } catch (error) {
+            // Manejar errores, por ejemplo, mostrar un mensaje de error
+            const errorMessage = (error as any).message || 'Error al registrar usuario. Por favor, inténtalo de nuevo.';
+            console.error('Error al registrar usuario:', errorMessage);
+            alert(errorMessage);
         }
-
-        // Puedes agregar aquí la lógica para manejar el formulario estático
-        alert('Formulario estático enviado. Puedes agregar la lógica correspondiente.');
     };
 
     return (
@@ -55,8 +104,28 @@ const Register = () => {
                             type="email"
                             className="w-full focus:outline-none"
                             placeholder="Correo electrónico"
-                            value={email}
-                            onChange={(e) => setEmail(e.target.value)}
+                            value={correo}
+                            onChange={(e) => setCorreo(e.target.value)}
+                        />
+                    </div>
+                    <div className="flex items-center border-b border-teal-500 py-2">
+                        <FaIdCard className="text-teal-500 mr-2" />
+                        <input
+                            type="number"
+                            className="w-full focus:outline-none"
+                            placeholder="DNI"
+                            value={dni}
+                            onChange={(e) => setDni(e.target.value)}
+                        />
+                    </div>
+                    <div className="flex items-center border-b border-teal-500 py-2">
+                        <FaPhoneSquareAlt className="text-teal-500 mr-2" />
+                        <input
+                            type="number"
+                            className="w-full focus:outline-none"
+                            placeholder="Teléfono "
+                            value={telefono}
+                            onChange={(e) => setTelefono(e.target.value)}
                         />
                     </div>
                     <div className="flex items-center border-b border-teal-500 py-2">
@@ -79,11 +148,11 @@ const Register = () => {
                             onChange={(e) => setConfirmPassword(e.target.value)}
                         />
                     </div>
-                    <Link to={"/Login"}>
-                    <Button variant='primary' className="w-full p-2 rounded mt-2">
+
+                    <Button variant="primary" className="w-full p-2 rounded mt-2" onClick={handleSubmit}>
                         Registrarse
                     </Button>
-                    </Link>
+
                 </form>
             </div>
         </div>
